dc0d0dfddc7a3bdba898916aa0969d041e01679d
krashchynskaya <karinaras97@yahoo.com>
2025-07-02 09:14:44 +0300

    feat: single property page

--- real-estate-properties-app/app.js
+++ real-estate-properties-app/app.js
@@ -2,29 +2,56 @@ import {propertiesForSale} from "./properties/list.js"
 
 const propertiesList = document.querySelector(".container");
 
-propertiesForSale.forEach(function (propertyForSale) {
+const urlParams = new URLSearchParams(window.location.search);
+let  singlePropertyId = parseInt(urlParams.get('id'));
 
-    let {propertyLocation, priceGBP, roomsM2, comment, image} = propertyForSale
+if(singlePropertyId)
+{
+    let propertyForSale = propertiesForSale.filter(property=>  property.id === singlePropertyId)
+
+    let {id, propertyLocation, type, priceGBP, roomsM2, comment, image} = propertyForSale[0]
 
     let propertyCard = document.createElement('section')
     propertyCard.className = 'card'
 
-    let singlePropertyId = "single-property" + Math.random()
-
     propertyCard.innerHTML = `
-        <img src="images/${image}" class="card img" alt="">
-       
-                              <div class="card-right">
-                               <button id="${singlePropertyId}" type="button"> <h2>${propertyLocation}</h2> </button
-                                 <h3>Price : ${priceGBP}</h3>
-                              </div>`
+    <img src="images/${image}" class="card img" alt="">
+      <div class="card-right">
+       <h2>${propertyLocation}</h2>
+       <h3>Price : ${priceGBP}</h3>
+       <p>${roomsM2} m2</p>
+       <p>${type}</p>
+       <p>${comment}</p>
+      </div>`
 
+    let backBtn = document.createElement('div')
+    backBtn.innerHTML = `<button class="my-button" onclick="history.back()">Go Back</button>`
 
     propertiesList.appendChild(propertyCard)
 
-    let singleProperty = document.getElementById(singlePropertyId)
+    propertiesList.appendChild(backBtn)
 
-    singleProperty.addEventListener('click', function (e) {
-        console.log('yes')
+}
+else{
+    propertiesForSale.forEach(function (propertyForSale) {
+
+        let {id, propertyLocation, type, priceGBP, roomsM2, comment, image} = propertyForSale
+
+        let propertyCard = document.createElement('section')
+        propertyCard.className = 'card'
+
+        propertyCard.innerHTML = `
+        <img src="images/${image}" class="card img" alt="">
+       
+                              <div class="card-right">
+                              
+                               <a href="?id=${id}">${propertyLocation}</a>
+                             
+                               <h3>Price : ${priceGBP}</h3>
+                              </div>`
+
+
+        propertiesList.appendChild(propertyCard)
     })
-})
+}
+
--- real-estate-properties-app/properties/list.js
+++ real-estate-properties-app/properties/list.js
@@ -1,29 +1,181 @@
 export const propertiesForSale = [
     {
+        id: 1,
         propertyLocation: 'Kensington, London',
+        type: "cottage",
         priceGBP: 890000,
-        roomsM2: [14, 18, 14, 10, 6],
+        roomsM2: 90,
         comment: 'Highly desirable location in stunning scenery!',
         image: 'cottage.jpg'
     },
     {
+        id: 2,
         propertyLocation: 'Wirral, Liverpool',
+        type: "house",
         priceGBP: 650000,
-        roomsM2: [18, 16, 15, 14, 17, 19, 9, 8],
+        roomsM2: 85,
         comment: 'Astonishing view with a modern finish!',
         image: 'house.jpg'
     },
     {
+        id: 3,
         propertyLocation: 'Beach, Brighton',
+        type: "villa",
+        priceGBP: 420000,
+        roomsM2: 190,
+        comment: 'Beautiful interior and a spacious room.',
+        image: 'villa.jpg'
+    },
+    {
+        id: 4,
+        propertyLocation: 'Highlands, Scotland',
+        type: "house",
+        priceGBP: 550000,
+        roomsM2: 52,
+        comment: 'Lots of potential, snug, a must see!',
+        image: 'house.jpg'
+    },
+    {
+        id: 5,
+        propertyLocation: 'Kensington, London',
+        type: "cottage",
+        priceGBP: 890000,
+        roomsM2: 87,
+        comment: 'Highly desirable location in stunning scenery!',
+        image: 'cottage.jpg'
+    },
+    {
+        id: 6,
+        propertyLocation: 'Wirral, Liverpool',
+        type: "house",
+        priceGBP: 650000,
+        roomsM2: 90,
+        comment: 'Astonishing view with a modern finish!',
+        image: 'house.jpg'
+    },
+    {
+        id: 7,
+        propertyLocation: 'Beach, Brighton',
+        type: "villa",
+        priceGBP: 420000,
+        roomsM2: 160,
+        comment: 'Beautiful interior and a spacious room.',
+        image: 'villa.jpg'
+    },
+    {
+        id: 8,
+        propertyLocation: 'Highlands, Scotland',
+        type: "house",
+        priceGBP: 550000,
+        roomsM2: 90,
+        comment: 'Lots of potential, snug, a must see!',
+        image: 'house.jpg'
+    },
+    {
+        id: 9,
+        propertyLocation: 'Kensington, London',
+        type: "cottage",
+        priceGBP: 890000,
+        roomsM2: 40,
+        comment: 'Highly desirable location in stunning scenery!',
+        image: 'cottage.jpg'
+    },
+    {
+        id: 10,
+        propertyLocation: 'Wirral, Liverpool',
+        type: "house",
+        priceGBP: 650000,
+        roomsM2: 90,
+        comment: 'Astonishing view with a modern finish!',
+        image: 'house.jpg'
+    },
+    {
+        id: 11,
+        propertyLocation: 'Beach, Brighton',
+        type: "villa",
+        priceGBP: 420000,
+        roomsM2: 90,
+        comment: 'Beautiful interior and a spacious room.',
+        image: 'villa.jpg'
+    },
+    {
+        id: 12,
+        propertyLocation: 'Highlands, Scotland',
+        type: "house",
+        priceGBP: 550000,
+        roomsM2: 33,
+        comment: 'Lots of potential, snug, a must see!',
+        image: 'house.jpg'
+    },
+    {
+        id: 13,
+        propertyLocation: 'Kensington, London',
+        type: "cottage",
+        priceGBP: 890000,
+        roomsM2: 90,
+        comment: 'Highly desirable location in stunning scenery!',
+        image: 'cottage.jpg'
+    },
+    {
+        id: 14,
+        propertyLocation: 'Wirral, Liverpool',
+        type: "house",
+        priceGBP: 650000,
+        roomsM2: 90,
+        comment: 'Astonishing view with a modern finish!',
+        image: 'house.jpg'
+    },
+    {
+        id: 15,
+        propertyLocation: 'Beach, Brighton',
+        type: "villa",
+        priceGBP: 420000,
+        roomsM2: 90,
+        comment: 'Beautiful interior and a spacious room.',
+        image: 'villa.jpg'
+    },
+    {
+        id: 16,
+        propertyLocation: 'Highlands, Scotland',
+        type: "house",
+        priceGBP: 550000,
+        roomsM2: 90,
+        comment: 'Lots of potential, snug, a must see!',
+        image: 'house.jpg'
+    },
+    {
+        id: 17,
+        propertyLocation: 'Kensington, London',
+        type: "cottage",
+        priceGBP: 890000,
+        roomsM2: 90,
+        comment: 'Highly desirable location in stunning scenery!',
+        image: 'cottage.jpg'
+    },
+    {
+        id: 18,
+        propertyLocation: 'Wirral, Liverpool',
+        type: "house",
+        priceGBP: 650000,
+        roomsM2: 90,
+        comment: 'Astonishing view with a modern finish!',
+        image: 'house.jpg'
+    },
+    {
+        id: 19,
+        propertyLocation: 'Beach, Brighton',
+        type: "villa",
         priceGBP: 420000,
         roomsM2: [5],
         comment: 'Beautiful interior and a spacious room.',
         image: 'villa.jpg'
     },
     {
+        id: 20,
         propertyLocation: 'Highlands, Scotland',
+        type: "house",
         priceGBP: 550000,
-        roomsM2: [6, 12, 11, 5],
+        roomsM2: 90,
         comment: 'Lots of potential, snug, a must see!',
         image: 'house.jpg'
     }
