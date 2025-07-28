80a7dec2b34ba7d209adf108ec2a4716559f0090
krashchynskaya <karinaras97@yahoo.com>
2025-07-28 22:37:51 +0300

    feat: implement meme generator

---
+++ meme-generator/src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img
+                 src='/troll-face.jpg'
+            />
+            <h1>Meme Generator</h1>
+        </header>
+    )
+}
