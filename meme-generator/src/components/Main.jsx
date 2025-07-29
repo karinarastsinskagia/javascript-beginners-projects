c305ad530fbf78a769a7c5958f9c6ce62d99991d
krashchynskaya <karinaras97@yahoo.com>
2025-07-29 10:29:26 +0300

    feat: updates

--- meme-generator/src/components/Main.jsx
+++ meme-generator/src/components/Main.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 export default function Main() {
 
@@ -8,12 +8,29 @@ export default function Main() {
         imageUrl: "http://i.imgflip.com/1bij.jpg"
     })
 
+    const [allMemes, setAllMemes] = useState([])
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, []);
+
     function handleChange(event) {
 
         const {value, name} = event.currentTarget
         setMeme(prevMeme => ({
             ...prevMeme,
             [name]: value
+        }))
+    }
+
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const newMemeUrl = allMemes[randomNumber].url
+        setMeme(prevState => ({
+            ...prevState,
+            imageUrl: newMemeUrl
         }))
     }
 
@@ -39,7 +56,7 @@ export default function Main() {
                         value={meme.bottomText}
                     />
                 </label>
-                <button>Get a new meme image 🖼</button>
+                <button onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
                 <img src={meme.imageUrl} />
